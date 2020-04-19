f4b998ce902f5ec11ea980ff768d34858c40e5ca
Daniel Pipkin <dpipkin@harding.edu>
2020-04-19 14:55:02 -0500

    Began on shop javascript
    Able to remove items and return peice values, will begin updating total shortly.

--- javascript/store.js
+++ javascript/store.js
@@ -1,1 +1,31 @@
-console.log("Here")
+var removeButton = document.querySelectorAll(".buttonRemove");
+
+for (var i = 0; i < removeButton.length; i++) {
+    var button = removeButton[i];
+    button.addEventListener("click", function(event) {
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove();
+        updateCartTotal();
+    });
+}
+
+function updateCartTotal() {
+    var cartContainer = document.querySelector('.cartItems');
+    var cartRows = cartContainer.querySelectorAll('.cartRow');
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.querySelectorAll('.cartPrice')[0];
+        var quantityElement = cartRow.querySelectorAll('.quantityInput');
+        console.log(priceElement, quantityElement);
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        console.log(price)
+        var cartTotal = document.querySelector('.totalPrice');
+        total = parseFloat(cartTotal.innerText.replace('$', ''));
+        total -= price;
+        console.log(total);
+        // need to finish by updating the total price and multiplying the price by the quantity in the cart row...
+
+    }
+
+
+}
